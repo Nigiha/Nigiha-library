ffc605f532ac4c1ea02d80bacc4ee2051d93ed8a
Nigiha <h_kohakusan589@icloud.com>
2025-11-25 14:06:59 +0900

    Binary serachを追加

---
+++ Atcoder/Binary search.py
@@ -0,0 +1,11 @@
+def Binary_search(dat, value):
+    left, right=0, len(dat)-1
+    while(left<=right):
+        mid=(left+right)//2
+        if dat[mid]==value:
+            return mid
+        elif dat[mid]<value:
+            left=mid+1
+        else:
+            right=mid-1
+    return -1
