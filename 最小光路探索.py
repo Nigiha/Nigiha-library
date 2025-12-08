8697813b2c23700f310d8b54538da82b758e910e
Nigiha <h_kohakusan589@icloud.com>
2025-12-08 17:45:22 +0900

    重み付け有向グラフを編集

--- 最小光路探索.py
+++ 最小光路探索.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib as plt
 plt.rcParams["backend"]="qtagg"
 
-#屈折率を決める
+#屈折率
 def refractive_index(line, row):
     n1=1.0
     n2=1.2
@@ -10,20 +10,24 @@ def refractive_index(line, row):
         return n1
     elif row>50:
         return n2
-
+#頂点(line, row)の配列上での位置
+def locate(line, row):
+    return N_row*line+row
 
 #重き付き有向グラフ
-line_total=100
-row_total=100
+N_line=4
+N_row=4
+N_node=N_line*N_row
 inf=1000.0
 edge=[(1,0), (3,1), (2,1), (3,2), (1,1), (2,3), (2,1), (1,3), (0,1)] #辺の方向
-g=[[[inf]*len(edge) for _ in range(row_total)] for _ in range(line_total)] #空グラフ
+gragh=[[inf]*N_node for _ in range(N_node)] #gragh[辺の始点][辺の終点]
 for i in range(len(edge)): #グラフの値の挿入
     dx=edge[i][0]
     dy=edge[i][1]
     actual_distance=np.sqrt(dx**2+dy**2)
-    for line in range(line_total-dy):
-        for row in range(row_total-dx):
-            g[line][row][i]=actual_distance*refractive_index(line, row)
+    for line in range(N_line-dy):
+        for row in range(N_row-dx):
+            gragh[locate(line, row)][locate(line+dy, row+dx)]=actual_distance*refractive_index(line, row)
 
 #経路探索(ダイクストラ法)
+
