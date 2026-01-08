cd80df75959fd1257179762bfd51e647e2bf38c7
Nigiha <h_kohakusan589@icloud.com>
2026-01-09 08:18:44 +0900

    水素原子E_0を求める変分法

---
+++ Physics/variational_method.py
@@ -0,0 +1,42 @@
+#代数計算ライブラリ
+import sympy as sp
+#数値計算ライブラリ
+from scipy.integrate import quad
+from scipy.optimize import minimize_scalar
+import numpy as np
+
+z, r=sp.symbols('z r', real=True, positive=True)
+#試行関数
+def trial_function(type):
+    if type=="Slater":
+        return sp.sqrt(z**3 / sp.pi)*sp.exp(-z*r)
+    elif type=="Gauss":
+        return (2*z/sp.pi)**sp.Rational(3, 4)*sp.exp(-z*r**2)
+    else:
+        print("error : not deffined type")
+        exit()
+psi=trial_function(type="Slater")
+
+#ハミルトニアンの作用(水素原子を考える)
+h_psi = -1/sp.S(2) * (1/r**2 * sp.diff(r**2 * sp.diff(psi, r), r)) - (1/r) * psi
+
+#被積分関数
+integrand_sym=4 * sp.pi * r**2 * psi * h_psi #ヤコビアン*<psi|H|psi>
+integrand_sci=sp.lambdify((r, z), integrand_sym, 'numpy')#代数計算sympyで求めたものを数値計算scipyで使えるように変換
+
+
+#-----↑代数計算-----↓数値計算-----
+
+
+#積分
+def calculate_energy(z_val):
+    val, err=quad(integrand_sci, 0, np.inf, args=(z_val))
+    return val
+
+#エネルギー期待値が最小になるように最適化
+result = minimize_scalar(calculate_energy, bounds=(0.1, 3.0), method='bounded')
+optimal_zeta = result.x
+min_energy = result.fun
+
+print("変分パラメータの最適値 : {:.5f}".format(optimal_zeta))
+print("基底エネルギーの近似値 : {:.5f} hartree".format(min_energy))
